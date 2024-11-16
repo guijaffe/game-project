2a4c56e1259300c8dc14b410584d15622f3f5090
alexander <zholobko_an@orlan.in>
2024-11-16 23:40:00 +0300

    modal and road

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-	// JSON данные слайдера
 	const icons = [
 		{ hasPlus: true, hasPerson: true },
 		{ hasPlus: false, hasPerson: true },
@@ -15,20 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
 	initSlider(icons);
 
 	function initSlider(iconsData) {
-		const friendsItems = document.querySelector('.friends-items');  // Новый контейнер для иконок
+		const friendsItems = document.querySelector('.friends-items');
 		let currentIndex = 0;
-		const maxVisible = 8; // Количество иконок, которые будут отображаться
+		const maxVisible = 8;
 		const totalIcons = iconsData.length;
 
 		// Функция для создания иконок
 		function createIcons() {
-			friendsItems.innerHTML = ''; // Очищаем контейнер перед добавлением
+			friendsItems.innerHTML = '';
 
 			_.map(iconsData, (icon) => {
 				const iconDiv = document.createElement('div');
 				iconDiv.classList.add('friends-icons');
 
-				// Добавляем классы plus и person, если они присутствуют
 				if (icon.hasPlus) {
 					const plusIcon = document.createElement('div');
 					plusIcon.classList.add('plus');
@@ -40,11 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
 					iconDiv.appendChild(personIcon);
 				}
 
-				friendsItems.appendChild(iconDiv); // Добавляем иконки в friends-items
+				friendsItems.appendChild(iconDiv);
 			});
 		}
 
-		// Функция для зацикленного перемещения иконок
 		function moveIcons() {
 			const iconsElements = friendsItems.querySelectorAll('.friends-icons');
 			const totalVisibleIcons = Math.min(maxVisible, iconsElements.length);
@@ -52,11 +49,9 @@ document.addEventListener('DOMContentLoaded', function() {
 			// Зацикливаем иконки
 			let iconsHTML = '';
 			for (let i = 0; i < totalVisibleIcons; i++) {
-				// Иконки, которые будут перемещаться
-				const index = (currentIndex + i) % totalIcons; // Зацикливаем индексы
+				const index = (currentIndex + i) % totalIcons;
 				const iconData = iconsData[index];
 
-				// Создаём HTML для иконок
 				let iconHTML = '';
 				if (iconData.hasPlus) {
 					iconHTML += '<div class="plus"></div>';
@@ -68,26 +63,22 @@ document.addEventListener('DOMContentLoaded', function() {
 				iconsHTML += `<div class="friends-icons">${iconHTML}</div>`;
 			}
 
-			// Обновляем содержимое контейнера с иконками
 			friendsItems.innerHTML = iconsHTML;
 		}
 
-		// Обработчик для кнопки влево с _.throttle для предотвращения частых срабатываний
 		const arrowLeft = document.querySelector('.arrow-left');
 		arrowLeft.addEventListener('click', _.throttle(function() {
 			currentIndex = (currentIndex === 0) ? totalIcons - 1 : currentIndex - 1;
 			moveIcons();
-		}, 300)); // Ограничиваем частоту кликов до одного в 300ms
+		}, 300));
 
-		// Обработчик для кнопки вправо с _.throttle
 		const arrowRight = document.querySelector('.arrow-right');
 		arrowRight.addEventListener('click', _.throttle(function() {
 			currentIndex = (currentIndex === totalIcons - 1) ? 0 : currentIndex + 1;
 			moveIcons();
-		}, 300)); // Ограничиваем частоту кликов до одного в 300ms
+		}, 300));
 
-		// Инициализация слайдера
-		createIcons(); // Создаём все иконки
-		moveIcons(); // Инициализируем слайдер
+		createIcons();
+		moveIcons();
 	}
 });
--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const app = new PIXI.Application({
 	backgroundColor: 0x159585,
 });
 
-// Загрузка фона с PixiJS
 PIXI.Assets.load('./assets/images/background.png').then((texture) => {
 	const background = new PIXI.Sprite(texture);
 	background.width = app.screen.width;
@@ -13,25 +12,24 @@ PIXI.Assets.load('./assets/images/background.png').then((texture) => {
 	app.stage.addChildAt(background, 0);
 });
 
-// Загрузка текстур карты и персонажа с PixiJS
 PIXI.Assets.load(['./assets/images/map.png', './assets/images/hero.png']).then((textures) => {
 	const map = new PIXI.Sprite(textures['./assets/images/map.png']);
 	app.stage.addChild(map);
 
 	const hero = new PIXI.Sprite(textures['./assets/images/hero.png']);
 	hero.x = 445;
-	hero.y = 490;
+	hero.y = 463;
 	hero.anchor.set(0.5);
 	app.stage.addChild(hero);
 
 	const points = [
-		{ x: 445, y: 490 },
-		{ x: 500, y: 450 },
-		{ x: 530, y: 400 },
-		{ x: 550, y: 350 },
-		{ x: 600, y: 300 }, // Универ
-		{ x: 650, y: 280 },
-		{ x: 700, y: 250 },
+		{ x: 445, y: 463 },
+		{ x: 351, y: 440 },
+		{ x: 277, y: 482 },
+		{ x: 190, y: 503 },
+		{ x: 110, y: 473 },
+		{ x: 123, y: 411 },
+		{ x: 142, y: 356 },
 	];
 
 	let isMoving = false;
@@ -39,33 +37,63 @@ PIXI.Assets.load(['./assets/images/map.png', './assets/images/hero.png']).then((
 	let t = 0;
 	let mirrorEffect = 1;
 
-	// Функция easing для плавного движения
 	function easeInOutQuad(t) {
 		return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 	}
 
-	// Функция для плавного движения
-	function moveToNextPoint() {
-		if (currentPointIndex >= points.length - 1) {
-			console.log('Достигнута конечная точка маршрута.');
-			displayCompletionMessage();
-			return;
-		}
+	const controlPointsList = [
+		[
+			{ x: 426, y: 473 },
+			{ x: 410, y: 418 },
+		],
+		[
+			{ x: 320, y: 463 },
+			{ x: 292, y: 481 },
+		],
+		[
+			{ x: 251, y: 502 },
+			{ x: 211, y: 507 },
+		],
+		[
+			{ x: 167, y: 504 },
+			{ x: 141, y: 498 },
+		],
+		[
+			{ x: 60, y: 445 },
+			{ x: 99, y: 427 },
+		],
+		[
+			{ x: 97, y: 385 },
+			{ x: 118, y: 364 },
+		]
+	];
 
+	function moveToNextPoint() {
 		const p0 = points[currentPointIndex];
-		const p1 = points[currentPointIndex + 1];
+		let p1 = points[currentPointIndex + 1];
 		t = 0;
 		isMoving = true;
 
-		// Анимация движения
 		function animateMove() {
 			if (t < 1 && isMoving) {
 				t += 0.02;
 
 				const easedT = easeInOutQuad(t);
 
-				hero.x = p0.x + (p1.x - p0.x) * easedT;
-				hero.y = p0.y + (p1.y - p0.y) * easedT;
+				const [controlPoint1, controlPoint2] = controlPointsList[currentPointIndex];
+
+				const bezierX = Math.pow(1 - easedT, 3) * p0.x
+					+ 3 * Math.pow(1 - easedT, 2) * easedT * controlPoint1.x
+					+ 3 * (1 - easedT) * Math.pow(easedT, 2) * controlPoint2.x
+					+ Math.pow(easedT, 3) * p1.x;
+
+				const bezierY = Math.pow(1 - easedT, 3) * p0.y
+					+ 3 * Math.pow(1 - easedT, 2) * easedT * controlPoint1.y
+					+ 3 * (1 - easedT) * Math.pow(easedT, 2) * controlPoint2.y
+					+ Math.pow(easedT, 3) * p1.y;
+
+				hero.x = bezierX;
+				hero.y = bezierY;
 
 				if (p1.x > p0.x) {
 					mirrorEffect = 1;
@@ -84,48 +112,43 @@ PIXI.Assets.load(['./assets/images/map.png', './assets/images/hero.png']).then((
 		animateMove();
 	}
 
-// select the open-btn button
-	let openBtn = document.getElementById('open-btn');
-// select the modal-background
-	let overlay = document.getElementById('overlay');
-// select the close-btn
-	let closeBtn = document.getElementById('close-btn');
-
-// shows the modal when the user clicks open-btn
-	openBtn.addEventListener('click', function() {
-		overlay.style.display = 'block';
-	});
-
-// hides the modal when the user clicks close-btn
-	closeBtn.addEventListener('click', function() {
-		overlay.style.display = 'none';
-	});
-
-// hides the modal when the user clicks outside the modal
-	window.addEventListener('click', function(event) {
-		// check if the event happened on the modal-background
-		if (event.target === overlay) {
-			// hides the modal
-			overlay.style.display = 'none';
-		}
-	});
-
-
-	// Обработчик клика на кнопку "Универ"
 	const handleButtonClick = _.throttle(() => {
 		if (!isMoving && currentPointIndex < points.length - 1) {
 			moveToNextPoint();
 		}
 	}, 300);
 
-	// Добавить обработчик для кнопки
 	const button = document.getElementById('move-button');
 	button.addEventListener('click', handleButtonClick);
 
-	// Анимация покачивания персонажа
 	function bounceAnimation() {
-		hero.y += Math.sin(Date.now() / 200) * 0.5;
+		hero.y += Math.sin(Date.now() / 200) * 0.2;
 		requestAnimationFrame(bounceAnimation);
 	}
 	bounceAnimation();
+
+	let openBtn = document.getElementById('open-btn');
+	let overlay = document.getElementById('overlay');
+	let closeBtn = document.getElementById('close-btn');
+
+	openBtn.addEventListener('click', function() {
+		overlay.style.display = 'block';
+	});
+
+	closeBtn.addEventListener('click', function() {
+		overlay.style.display = 'none';
+	});
+
+	window.addEventListener('click', function(event) {
+		if (event.target === overlay) {
+			overlay.style.display = 'none';
+		}
+	});
+
+	app.stage.interactive = true;
+	app.stage.on('pointerdown', (event) => {
+		const position = event.data.global;
+		console.log(`Координаты клика: x = ${position.x}, y = ${position.y}`);
+	});
+
 });
